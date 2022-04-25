30f91ff4402c885f3abb790bec63e7ac99190c61
harryyo <56394038+harryyo@users.noreply.github.com>
2022-04-25 15:09:03 +0100

    Commented test removed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -42,22 +42,6 @@ def test_msal_get_token_returns_404(flask_test_client):
     assert response.get_json()["message"] == "No valid token"
 
 
-# def test_msal_get_token_returns_valid_token(flask_test_client, mocker):
-#     """
-#     GIVEN We have a functioning Authenticator API
-#     WHEN a GET request for /auth/msal/get-token
-#     THEN we should receive a 200 response with token
-#     """
-#     session_mock = mocker.patch.object(flask, "session")
-#     mock_token = {"mock": "token"}
-#     session_mock.get.return_value = {"user": mock_token}
-#     endpoint = "/auth/msal/get-token"
-#     response = flask_test_client.get(endpoint)
-#
-#     assert response.status_code == 404
-#     assert response.get_json()["mock"] == "token"
-
-
 def test_msal_graphcall_returns_404(flask_test_client):
     """
     GIVEN We have a functioning Authenticator API
