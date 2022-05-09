d3e7811cf7aaa5d1d0ec5e1860b6150b46294778
harryyo <56394038+harryyo@users.noreply.github.com>
2022-05-09 17:09:22 +0100

    applications attribute added to Account model

--- api/session/models/account.py
+++ api/session/models/account.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class Account(object):
     id: str
     email: str
+    applications: List[str]
 
     @staticmethod
     def from_json(data: dict):
         return Account(
             id=data.get("id"),
             email=data.get("email"),
+            applications=data.get("applications"),
         )
