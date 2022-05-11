7d011ea90c0fdceacb49e734f239f78ad2bd744e
harryyo <56394038+harryyo@users.noreply.github.com>
2022-05-11 10:11:21 +0100

    Application model added, and updated as child of Account applications list type hint

--- api/session/models/account.py
+++ api/session/models/account.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 from typing import List
 
+from api.session.models.application import Application
+
 
 @dataclass
 class Account(object):
     id: str
     email: str
-    applications: List[str]
+    applications: List[Application]
 
     @staticmethod
     def from_json(data: dict):
--- api/session/models/application.py
+++ api/session/models/application.py
@@ -1,17 +1,12 @@
 from dataclasses import dataclass
-from typing import List
 
 
 @dataclass
-class Account(object):
+class Application(object):
     id: str
-    email: str
-    applications: List[str]
 
     @staticmethod
     def from_json(data: dict):
-        return Account(
+        return Application(
             id=data.get("id"),
-            email=data.get("email"),
-            applications=data.get("applications"),
         )
