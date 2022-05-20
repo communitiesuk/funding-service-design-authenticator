c79d3ebd2d83aaf358fd07dab0dfbe23263bc5e3
harryyo <56394038+harryyo@users.noreply.github.com>
2022-05-20 18:25:20 +0100

    Update default config to use MAGIC_LINK_REDIRECT_URL env variable if set

--- config/environments/default.py
+++ config/environments/default.py
@@ -108,12 +108,13 @@ class Config(object):
     )
     MAGIC_LINK_REDIRECT_HOST = APPLICANT_FRONTEND_HOST
     MAGIC_LINK_REDIRECT_URL = environ.get("MAGIC_LINK_REDIRECT_URL")
-    if not MAGIC_LINK_REDIRECT_URL and MAGIC_LINK_REDIRECT_HOST:
-        MAGIC_LINK_REDIRECT_URL = (
-            MAGIC_LINK_REDIRECT_HOST + "/account/{account_id}"
-        )
-    else:
-        MAGIC_LINK_REDIRECT_URL = "https://www.gov.uk/error"
+    if not MAGIC_LINK_REDIRECT_URL:
+        if MAGIC_LINK_REDIRECT_HOST:
+            MAGIC_LINK_REDIRECT_URL = (
+                MAGIC_LINK_REDIRECT_HOST + "/account/{account_id}"
+            )
+        else:
+            MAGIC_LINK_REDIRECT_URL = "https://www.gov.uk/error"
     MAGIC_LINK_RECORD_PREFIX = "link"
     MAGIC_LINK_USER_PREFIX = "account"
 
