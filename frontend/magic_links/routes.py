14e89c20a3672c93995b87be873d84d39c7a9975
endamccormack <endamccormack0@gmail.com>
2022-08-10 11:56:47 +0100

    reworked to use globals

--- frontend/magic_links/routes.py
+++ frontend/magic_links/routes.py
@@ -9,8 +9,6 @@ from models.account import AccountMethods
 from models.magic_link import MagicLinkError
 from models.notification import NotificationError
 
-from config import Config
-
 magic_links_bp = Blueprint(
     "magic_links_bp",
     __name__,
@@ -24,18 +22,14 @@ def invalid():
 
     return (
         render_template(
-            "invalid.html", new_magic_link_url=url_for("magic_links_bp.new"),
-            accessibility_statement_url=Config.APPLICANT_FRONTEND_ACCESSIBILITY_STATEMENT_URL,
-            cookie_policy_url=Config.APPLICANT_FRONTEND_COOKIE_POLICY_URL), 403,
+            "invalid.html", new_magic_link_url=url_for("magic_links_bp.new")), 403,
     )
 
 
 @magic_links_bp.route("/landing/<link_id>", methods=["GET"])
 def landing(link_id):
 
-    return render_template("landing.html", link_id=link_id,
-                           accessibility_statement_url=Config.APPLICANT_FRONTEND_ACCESSIBILITY_STATEMENT_URL,
-                           cookie_policy_url=Config.APPLICANT_FRONTEND_COOKIE_POLICY_URL)
+    return render_template("landing.html", link_id=link_id)
 
 
 @magic_links_bp.route("/new", methods=["GET", "POST"])
@@ -76,9 +70,7 @@ def new():
         except AccountError as e:
             form.email.errors.append(str(e.message))
 
-    return render_template("email.html", form=form, fund_round=fund_round,
-                           accessibility_statement_url=Config.APPLICANT_FRONTEND_ACCESSIBILITY_STATEMENT_URL,
-                           cookie_policy_url=Config.APPLICANT_FRONTEND_COOKIE_POLICY_URL)
+    return render_template("email.html", form=form, fund_round=fund_round)
 
 
 @magic_links_bp.route("/check-email", methods=["GET"])
@@ -88,6 +80,4 @@ def check_email():
     inbox for an email with a magic link
     """
 
-    return render_template("check_email.html", email=request.args.get("email"),
-                           accessibility_statement_url=Config.APPLICANT_FRONTEND_ACCESSIBILITY_STATEMENT_URL,
-                           cookie_policy_url=Config.APPLICANT_FRONTEND_COOKIE_POLICY_URL)
+    return render_template("check_email.html", email=request.args.get("email"))
