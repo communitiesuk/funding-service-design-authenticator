17fcb68b875b9e32bf8c1b1dabda409cd36491d8
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-08-18 12:00:44 +0100

    updated check name

--- tests/test_healthchecks.py
+++ tests/test_healthchecks.py
@@ -6,7 +6,7 @@ class TestHealthchecks:
     @pytest.mark.usefixtures("mock_redis_magic_links")
     def testChecks(self, flask_test_client):
         response = flask_test_client.get("/healthcheck")
-        expected_dict = {"checks":[{"check_running":"OK"}, {"check_redis": "OK"}]}
+        expected_dict = {"checks":[{"check_flask_running":"OK"}, {"check_redis": "OK"}]}
         assert response.status_code == 200, "Unexpected response code"
         assert response.json == expected_dict, "Unexpected json body"
 
