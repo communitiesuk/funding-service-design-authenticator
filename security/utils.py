b242e82037cf2d038ad40ccb9e4edd5100bda5d6
harryyo <56394038+harryyo@users.noreply.github.com>
2022-08-24 13:25:29 +0100

    New sessions/sign-out route added with service/magic-links/signed-out page for redirection back to create a new magic link. Models/magic_link.py refactored to facilitate this

--- security/utils.py
+++ security/utils.py
@@ -1,10 +1,21 @@
 import jwt
 from config import Config
 
+algorithm = "RS256"
+
 
 def create_token(payload):
-    return jwt.encode(payload, Config.RSA256_PRIVATE_KEY, algorithm="RS256")
+    return jwt.encode(payload, Config.RSA256_PRIVATE_KEY, algorithm=algorithm)
 
 
 def validate_token(token):
-    return jwt.decode(token, Config.RSA256_PUBLIC_KEY, algorithms=["RS256"])
+    return jwt.decode(token, Config.RSA256_PUBLIC_KEY, algorithms=[algorithm])
+
+
+def decode_with_options(token, options: dict):
+    return jwt.decode(
+        token,
+        Config.RSA256_PUBLIC_KEY,
+        algorithms=[algorithm],
+        options=options,
+    )
