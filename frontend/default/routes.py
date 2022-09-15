dd4967e97156a3919b9aa5a38f3e660b14963e57
Steve Walton <steve.walton@evoco.co.uk>
2022-09-15 12:12:52 +0100

    Fixing missing arg value

--- frontend/default/routes.py
+++ frontend/default/routes.py
@@ -24,4 +24,9 @@ def not_found(error):
 @default_bp.errorhandler(500)
 def internal_server_error(error):
     current_app.logger.error(f"Encountered 500: {error}")
-    return render_template("500.html"), 500
+    return (
+        render_template(
+            "500.html", contact_us_url=Config.APPLICANT_FRONTEND_CONTACT_US_URL
+        ),
+        500,
+    )
