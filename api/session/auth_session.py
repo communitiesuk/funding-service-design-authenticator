6ff29c32aa70f96e3671ea33994faabd48422d1c
harryyo <56394038+harryyo@users.noreply.github.com>
2022-09-23 19:59:39 +0100

    Revert clear_session cookie settings

--- api/session/auth_session.py
+++ api/session/auth_session.py
@@ -84,9 +84,6 @@ class AuthSessionView(MethodView):
             "",
             domain=Config.COOKIE_DOMAIN,
             expires=0,
-            secure=Config.SESSION_COOKIE_SECURE,
-            samesite=Config.SESSION_COOKIE_SAMESITE,
-            httponly=Config.SESSION_COOKIE_HTTPONLY,
         )
         return response
 
