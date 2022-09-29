c15134c4d4eb784b8ff64a7c5e535a81c96d25d4
harryyo <56394038+harryyo@users.noreply.github.com>
2022-09-29 11:52:00 +0100

    Add tests, mocks and updated test api_data to test auto creation of application forms and toggle of functionality with config var

--- tests/mocks/__init__.py
+++ tests/mocks/__init__.py
@@ -5,3 +5,4 @@ from tests.mocks.msal import mock_msal_client_application  # noqa
 from tests.mocks.redis_magic_links import mock_redis_magic_links  # noqa
 from tests.mocks.redis_sessions import mock_redis_sessions  # noqa
 from tests.mocks.selenium_chrome_driver import selenium_chrome_driver  # noqa
+from tests.mocks.application_methods import mock_create_application # noqa
--- tests/mocks/application_methods.py
+++ tests/mocks/application_methods.py
@@ -1,44 +1,22 @@
-from typing import List
-from typing import Optional
-
 import pytest
-
-id_token_claims = {"name": "Mr Test", "id": 123}
-accounts = [{"username": "test"}]
+from models.application import Application
 
 
-class Account(dict):
-    pass
+class MockApplicationMethods(object):
 
-
-class ConfidentialClientApplication(object):
-    def acquire_token_by_auth_code_flow(
-        self, auth_code_flow, auth_response, scopes=None, **kwargs
-    ):
-        return {
-            "id_token": 1,
-            "access_token": 2,
-            "id_token_claims": id_token_claims,
-        }
-
-    def get_accounts(self, username=None):
-        return accounts
-
-    def acquire_token_silent(
-        self,
-        scopes,  # type: List[str]
-        account,  # type: Optional[Account]
-        authority=None,  # See get_authorization_request_url()
-        force_refresh=False,  # type: Optional[bool]
-        claims_challenge=None,
-        **kwargs,
-    ):
-        return [{"username": "test"}]
+    @staticmethod
+    def create_application():
+        return Application.from_json({
+            "application_id": "dummy-application-id",
+            "fund_name": "dummy-fund-name",
+            "fund_id": "dummy-fund-id",
+            "round_id": "dummy-round-id",
+        })
 
 
 @pytest.fixture()
-def mock_msal_client_application(mocker):
+def mock_create_application(mocker):
     mocker.patch(
-        "msal.ConfidentialClientApplication.acquire_token_by_auth_code_flow",
-        ConfidentialClientApplication.acquire_token_by_auth_code_flow,
+        "models.application.ApplicationMethods.create_application",
+        MockApplicationMethods.create_application,
     )
