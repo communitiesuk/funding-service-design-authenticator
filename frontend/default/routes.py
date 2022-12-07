94c6100b3a4e1f58f91492a26247cfaf3b6d87ea
harryyo <56394038+harryyo@users.noreply.github.com>
2022-12-07 14:52:18 +0000

    Add default login/logout functioanlity to root page for user verification

--- frontend/default/routes.py
+++ frontend/default/routes.py
@@ -2,14 +2,25 @@ from config import Config
 from flask import Blueprint
 from flask import current_app
 from flask import render_template
+from flask import g
 from models.data import get_round_data_fail_gracefully
+from fsd_utils.authentication.decorators import login_requested
 
 default_bp = Blueprint("default_bp", __name__, template_folder="templates")
 
 
 @default_bp.route("/")
+@login_requested
 def index():
-    return render_template("index.html")
+    logged_in_user = None
+    if g.is_authenticated:
+        logged_in_user = g.user
+    return render_template(
+        "index.html",
+        logged_in_user=logged_in_user,
+        login_url=Config.SSO_LOGIN_ENDPOINT,
+        logout_url=Config.SSO_LOGOUT_ENDPOINT,
+    )
 
 
 @default_bp.errorhandler(404)
