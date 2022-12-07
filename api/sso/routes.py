ec15024d297e5987a9beac7edb1f72fd5208a2a2
harryyo <56394038+harryyo@users.noreply.github.com>
2022-12-07 14:50:48 +0000

    Update get_token endpoint to set fsd_user_token cookie with required sso auth claims

--- api/sso/routes.py
+++ api/sso/routes.py
@@ -1,4 +1,5 @@
 import warnings
+import json
 
 import msal
 import requests
@@ -6,7 +7,10 @@ from config import Config
 from flask import redirect
 from flask import request
 from flask import session
+from flask import current_app
 from flask.views import MethodView
+from models.account import AccountMethods
+from api.session.auth_session import AuthSessionView
 
 
 class SsoView(MethodView):
@@ -61,11 +65,25 @@ class SsoView(MethodView):
                 return result, 500
             session["user"] = result.get("id_token_claims")
             self._save_cache(cache)
-            # TODO: Set an fsd-user-token for the user here
-            return session["user"], 200
         except ValueError as e:  # Usually caused by CSRF
             warnings.warn(f"Value Error on get_token route: {str(e)}")
-        return {"message": "No valid token"}, 404
+
+        if not "user" in session or not session["user"].get("sub"):
+            return {"message": "No valid token"}, 404
+
+        updated_account = AccountMethods.create_or_update_account(
+            azure_ad_subject_id=session["user"].get("sub"),
+            email = session["user"].get("preferred_username"),
+            full_name = session["user"].get("name"),
+            roles = session["user"].get("roles")
+        )
+
+        # Create session token, set cookie and redirect
+        return AuthSessionView.create_session_and_redirect(
+            account=updated_account,
+            redirect_url=Config.ASSESSMENT_POST_LOGIN_URL,
+            timeout_seconds=Config.FSD_ASSESSMENT_SESSION_TIMEOUT_SECONDS
+        )
 
     def graph_call(self):
         """
