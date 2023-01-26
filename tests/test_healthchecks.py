40ea620b2f4b21878acc157deb1af7d034ad3430
srh-sloan <srh-sloan@users.noreply.github.com>
2023-01-26 10:58:45 +0000

    getting default IDs from utils (#114)
    * getting default IDs from utils
    
    * fixing unit test
    
    * utils version

--- tests/test_healthchecks.py
+++ tests/test_healthchecks.py
@@ -6,7 +6,8 @@ class TestHealthchecks:
     def testChecks(self, flask_test_client):
         response = flask_test_client.get("/healthcheck")
         expected_dict = {
-            "checks": [{"check_flask_running": "OK"}, {"check_redis": "OK"}]
+            "checks": [{"check_flask_running": "OK"}, {"check_redis": "OK"}],
+            "version": "abc123",
         }
         assert response.status_code == 200, "Unexpected response code"
         assert response.json == expected_dict, "Unexpected json body"
