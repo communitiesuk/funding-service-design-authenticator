a19d1100510491d11d411b5f3a9b22f315dc8972
tferns <thomas.ferns@version1.com>
2023-03-16 13:09:35 +0000

    BAU: Pre-commit maintenance

--- testing/mocks/mocks/__init__.py
+++ testing/mocks/mocks/__init__.py
@@ -1,8 +1,14 @@
 # flake8 : noqa
-from testing.mocks.mocks.application_methods import mock_create_application  # noqa
+from testing.mocks.mocks.application_methods import (  # noqa
+    mock_create_application,  # noqa
+)  # noqa
 from testing.mocks.mocks.flask_test_client import flask_test_client  # noqa
 from testing.mocks.mocks.live_server import app  # noqa
 from testing.mocks.mocks.msal import mock_msal_client_application  # noqa
-from testing.mocks.mocks.redis_magic_links import mock_redis_magic_links  # noqa
+from testing.mocks.mocks.redis_magic_links import (  # noqa
+    mock_redis_magic_links,  # noqa
+)  # noqa
 from testing.mocks.mocks.redis_sessions import mock_redis_sessions  # noqa
-from testing.mocks.mocks.selenium_chrome_driver import selenium_chrome_driver  # noqa
+from testing.mocks.mocks.selenium_chrome_driver import (  # noqa
+    selenium_chrome_driver,  # noqa
+)  # noqa
