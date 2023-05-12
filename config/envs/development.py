84f0c264894f60f50c73632a775367d304a76c7e
Gio Karunakaran <gio.karunakaran@version1.com>
2023-05-12 17:23:31 +0100

    fs-2691-local-vs-test-bug

--- config/envs/development.py
+++ config/envs/development.py
@@ -46,19 +46,21 @@ class DevelopmentConfig(Config):
     SESSION_USE_SIGNER = True
     SESSION_COOKIE_SECURE = False
 
+    # Uncomment to use auth locally the old way (preset pub and priv keys aka
+    # not decoding from base64 envvars):
     # RSA 256 KEYS
-    if not hasattr(Config, "RSA256_PRIVATE_KEY"):
-        _test_private_key_path = (
-            Config.FLASK_ROOT + "/tests/keys/rsa256/private.pem"
-        )
-        with open(_test_private_key_path, mode="rb") as private_key_file:
-            RSA256_PRIVATE_KEY = private_key_file.read()
-    if not hasattr(Config, "RSA256_PUBLIC_KEY"):
-        _test_public_key_path = (
-            Config.FLASK_ROOT + "/tests/keys/rsa256/public.pem"
-        )
-        with open(_test_public_key_path, mode="rb") as public_key_file:
-            RSA256_PUBLIC_KEY = public_key_file.read()
+    # if not hasattr(Config, "RSA256_PRIVATE_KEY"):
+    #     _test_private_key_path = (
+    #         Config.FLASK_ROOT + "/tests/keys/rsa256/private.pem"
+    #     )
+    #     with open(_test_private_key_path, mode="rb") as private_key_file:
+    #         RSA256_PRIVATE_KEY = private_key_file.read()
+    # if not hasattr(Config, "RSA256_PUBLIC_KEY"):
+    #     _test_public_key_path = (
+    #         Config.FLASK_ROOT + "/tests/keys/rsa256/public.pem"
+    #     )
+    #     with open(_test_public_key_path, mode="rb") as public_key_file:
+    #         RSA256_PUBLIC_KEY = public_key_file.read()
 
     # Redis
 
