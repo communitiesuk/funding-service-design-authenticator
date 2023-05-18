9ccbfefa9511a76f24aae85f7144bdcec2944f5e
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2023-05-18 15:20:41 +0100

    PAB-327: post-award compatibility

--- frontend/sso/routes.py
+++ frontend/sso/routes.py
@@ -1,6 +1,8 @@
 from config import Config
 from flask import Blueprint
 from flask import render_template
+from flask import request
+from flask import url_for
 
 sso_bp = Blueprint(
     "sso_bp",
@@ -12,11 +14,14 @@ sso_bp = Blueprint(
 
 @sso_bp.route("/signed-out/<status>")
 def signed_out(status):
+    return_app = request.args.get("return_app")
     return (
         render_template(
             "sso_signed_out.html",
             status=status,
-            login_url=Config.SSO_LOGIN_ENDPOINT,
+            login_url=url_for(
+                Config.SSO_LOGIN_ENDPOINT, return_app=return_app
+            ),
         ),
         200,
     )
