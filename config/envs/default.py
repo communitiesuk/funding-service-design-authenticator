93f3523a24a671f8ee6359d4aaa70d612e4f57fa
srh-sloan <srh-sloan@users.noreply.github.com>
2023-05-30 09:32:52 +0100

    Making privacy notice url dynamic (#161)

--- config/envs/default.py
+++ config/envs/default.py
@@ -137,6 +137,7 @@ class DefaultConfig(object):
         APPLICANT_FRONTEND_HOST + "/cookie_policy"
     )
     APPLICANT_FRONTEND_CONTACT_US_URL = APPLICANT_FRONTEND_HOST + "/contact_us"
+    APPLICANT_FRONTEND_PRIVACY_URL = APPLICANT_FRONTEND_HOST + "/privacy"
     APPLICATION_ALL_QUESTIONS_URL = (
         APPLICANT_FRONTEND_HOST
         + "/all_questions/{fund_short_name}/{round_short_name}"
