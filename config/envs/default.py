b11019ea5a3d42b811999031908210be8350f879
Kurt Plaschke <kurt.plaschke@version1.com>
2023-10-31 16:20:49 +0000

    removed sha

--- config/envs/default.py
+++ config/envs/default.py
@@ -215,7 +215,6 @@ class DefaultConfig(object):
             "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
             "'sha256-l1eTVSK8DTnK8+yloud7wZUqFrI0atVo6VlC6PJvYaQ='",
             "'sha256-RgdCrr7A9yqYVstE6QiM/9RNRj4bYipcUa2C2ywQT1A='",
-            "'sha256-wKG/TnoSB5q5sH0k8ISE3N4M0RgLH3j2AbF6bKlIEeQ='"
             "https://tagmanager.google.com",
             "https://www.googletagmanager.com",
             "https://*.google-analytics.com",
