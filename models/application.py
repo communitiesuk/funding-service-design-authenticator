3442cc094fa6ae120f9bcf9768e29b06998c0379
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2024-10-30 10:21:39 +0000

    remove unused code

--- models/application.py
+++ models/application.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
-from typing import List
 
 from config import Config
-from models.data import get_data
 from models.data import post_data
 
 
@@ -23,58 +21,7 @@ class Application(object):
         )
 
 
-class ApplicationError(Exception):
-    """Exception raised for errors in Application management
-
-    Attributes:
-        message -- explanation of the error
-    """
-
-    def __init__(self, message="Sorry, there was a problem, please try later"):
-        self.message = message
-        super().__init__(self.message)
-
-
 class ApplicationMethods(Application):
-    @staticmethod
-    def get_application(application_id: str) -> Application | None:
-        """
-        Get application from the application store with application_id.
-
-        Args:
-            application_id (str): The application_id
-
-        Returns:
-            List of Applications
-        """
-
-        url = Config.APPLICATION_STORE_API_HOST + Config.APPLICATION_STORE_APPLICATION_ENDPOINT
-        params = {"application_id": application_id}
-        response = get_data(url, params)
-
-        if response and "application_id" in response:
-            return Application.from_json(response)
-
-    @staticmethod
-    def get_applications(account_id: str, **kwargs) -> List[Application] | None:
-        """
-        Get applications from the application store matching given params.
-
-        Args:
-            account_id (str): The account id.
-
-        Returns:
-            List of Applications
-        """
-        if account_id is None:
-            raise TypeError("Requires an account_id")
-
-        url = Config.APPLICATION_STORE_API_HOST + Config.APPLICATION_STORE_APPLICATIONS_ENDPOINT
-        response = get_data(url, kwargs)
-
-        if response:
-            return [Application.from_json(item) for item in response]
-
     @staticmethod
     def create_application(account_id: str, fund_id: str, round_id: str) -> Application | None:
         """
